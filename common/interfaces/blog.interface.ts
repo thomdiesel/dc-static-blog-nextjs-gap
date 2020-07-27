2bb0aee79ded12ad902e36c21ba5a1f8da47892d
tarmstrong@amplience.com <tarmstrong@amplience.com>
2020-07-27 09:45:05 -0400

    Update blog.interface.ts

--- common/interfaces/blog.interface.ts
+++ common/interfaces/blog.interface.ts
@@ -1,14 +1,11 @@
 import { DefaultContentBody } from 'dc-delivery-sdk-js';
-import { AmplienceContent } from './content.type';
 
 export interface Blog extends DefaultContentBody {
   title: string;
   heading: string;
-  description: string;
   searchPlaceHolder: string;
-  content: AmplienceContent[];
 }
 
 export const isBlog = (blog: DefaultContentBody | Blog): blog is Blog => {
-  return blog.title !== undefined && blog.heading !== undefined && blog.description !== undefined;
+  return blog.title !== undefined && blog.heading !== undefined;
 };
