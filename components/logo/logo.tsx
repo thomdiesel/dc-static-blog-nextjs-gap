4e43f1a46664d021bebed80fceeea0bcab150e2f
thomdiesel <38358017+thomdiesel@users.noreply.github.com>
2020-09-02 07:53:58 -0400

    Update logo.tsx

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import theme from '../../common/styles/default/theme';
 
 const Logo = ({ darkMode }: { darkMode?: boolean }): ReactElement => {
-  const logoSrcName = darkMode ? 'athletalogo' : 'athletalogo';
+  const logoSrcName = darkMode ? 'logo' : 'logo';
   return (
     <>
       <img
